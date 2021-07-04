ff24af824a54cc8d5a93d8c2a98fd43940c86bfe
Melissa Naugle <mnaugle@csumb.edu>
2021-07-04 00:15:45 -0700

    new gene list comparisons, GO_MWU, WGCNAs

--- GO/GO_MWU/GO_MWU.R
+++ GO/GO_MWU/GO_MWU.R
@@ -15,13 +15,13 @@
 
 ################################################################
 # First, press command-D on mac or ctrl-shift-H in Rstudio and navigate to the directory containing scripts and input files. Then edit, mark and execute the following bits of code, one after another.
-setwd("~/Desktop/GitHub/RNAseq_allsites_Barshisreference/GO_MWU/")
+setwd("~/Desktop/GitHub/RNAseq_allsites_Barshisreference/GO/GO_MWU/")
 
 # Edit these to match your data file names: 
-input="contigs_signif_file_GOMWU_DE_control_highvlowpollution.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+input="contigs_signif_file_GOMWU_all_heat_v_control_logfoldchange.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
 goAnnotations="GO_annot_table" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
-goDivision="BP" # either MF, or BP, or CC
+goDivision="MF" # either MF, or BP, or CC
 source("gomwu.functions.R")
 
 # ------------- Calculating stats
@@ -29,9 +29,9 @@ source("gomwu.functions.R")
 
 gomwuStats(input, goDatabase, goAnnotations, goDivision,
 	perlPath="perl", # replace with full path to perl executable if it is not in your system's PATH already
-	largest=0.3,  # a GO category will not be considered if it contains more than this fraction of the total number of genes
+	largest=0.1,  # a GO category will not be considered if it contains more than this fraction of the total number of genes
 	#originally largest = 0.1
-	smallest=1,   # a GO category should contain at least this many genes to be considered, 
+	smallest=5,   # a GO category should contain at least this many genes to be considered, 
 	#originally smallest = 5
 	clusterCutHeight=0.25, # threshold for merging similar (gene-sharing) terms. See README for details.
   #originally clustercutheight = 0.25 
@@ -48,13 +48,13 @@ quartz()
 results=gomwuPlot(input,goAnnotations,goDivision,
 	absValue=-log(0.05,10),  # genes with the measure value exceeding this will be counted as "good genes". This setting is for signed log-pvalues. Specify absValue=0.001 if you are doing Fisher's exact test for standard GO enrichment or analyzing a WGCNA module (all non-zero genes = "good genes").
 #	absValue=1, # un-remark this if you are using log2-fold changes
-	level1=0.1, # FDR threshold for plotting. Specify level1=1 to plot all GO categories containing genes exceeding the absValue.
-	level2=0.05, # FDR cutoff to print in regular (not italic) font.
-	level3=0.01, # FDR cutoff to print in large bold font.
+	level1=0.005, # FDR threshold for plotting. Specify level1=1 to plot all GO categories containing genes exceeding the absValue.
+	level2=0.001, # FDR cutoff to print in regular (not italic) font.
+	level3=0.0001, # FDR cutoff to print in large bold font.
 	txtsize=0.8,    # decrease to fit more on one page, or increase (after rescaling the plot so the tree fits the text) for better "word cloud" effect
 	treeHeight=0.5, # height of the hierarchical clustering tree
-	colors=c("black","firebrick1","black","lightcoral") # these are default colors, un-remar and change if needed
-)
+	colors=c("dodgerblue2","firebrick1","skyblue2","lightcoral") # these are default colors, un-remar and change if needed
+) 
 # manually rescale the plot so the tree matches the text 
 # if there are too many categories displayed, try make it more stringent with level1=0.05,level2=0.01,level3=0.001.  
 
